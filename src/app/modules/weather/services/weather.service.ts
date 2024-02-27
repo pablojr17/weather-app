c6dfef51481828fc23882f2d7464a7c5e6f916c7
Pablo Gomes <pablojr17@gmail.com>
2024-02-26 21:59:05 -0300

    criando service

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private apiKey = '8d0be5d3a58b9a7547269555b7ef4b20';
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
+}
