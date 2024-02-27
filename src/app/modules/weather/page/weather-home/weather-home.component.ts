c8dd420a2dbecdf4005412ac01c93920d8f94ac1
Pablo Gomes <pablojr17@gmail.com>
2024-02-26 22:18:39 -0300

    Adiiconando imagens e consumindo service

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/Weather';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: ['./weather-home.component.scss']
+  styleUrls: ['./weather-home.component.scss'],
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'Cotegipe';
+  weatherDatas!: WeatherDatas;
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName).subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+        console.log(this.weatherDatas);
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
